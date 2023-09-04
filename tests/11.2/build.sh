ba3becc79d93802a40cb9b203bab97c39c600baa
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-09-04 15:44:26 +0530

    Update build.sh

--- tests/11.2/build.sh
+++ tests/11.2/build.sh
@@ -1,3 +1,3 @@
 cp -rf 11.2/* .
-sed sed -i "s~ARG REPOSITORY="http://archive.mariadb.org/mariadb-11.2.0/repo/ubuntu/ jammy main main/debug"~ARG REPOSITORY="https://archive.mariadb.org/mariadb-11.2.1/repo/ubuntu/ jammy main main/debug"~g" Dockerfile
+sed -i "s~ARG REPOSITORY="http://archive.mariadb.org/mariadb-11.2.0/repo/ubuntu/ jammy main main/debug"~ARG REPOSITORY="https://archive.mariadb.org/mariadb-11.2.1/repo/ubuntu/ jammy main main/debug"~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/mariadb:11.2 | docker load
