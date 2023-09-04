dc56ef2a1083e41379d9341be1c7f8e81da1cc75
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-09-04 16:43:59 +0530

    Update build.sh

--- tests/11.2/build.sh
+++ tests/11.2/build.sh
@@ -1,3 +1,5 @@
 cp -rf 11.2/* .
 sed -i "s~ARG REPOSITORY="http://archive.mariadb.org/mariadb-11.2.0/repo/ubuntu/ jammy main main/debug"~ARG REPOSITORY="https://archive.mariadb.org/mariadb-11.2.1/repo/ubuntu/ jammy main main/debug"~g" Dockerfile
+sed -i "s~org.opencontainers.image.version="11.2.0" \~org.opencontainers.image.version="11.2.1" \~g" Dockerfile
+sed -i "s~ARG MARIADB_VERSION=1:11.2.0+maria~ubu2204~ARG MARIADB_VERSION=1:11.2.1+maria~ubu2204~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/mariadb:11.2 | docker load
